341300980db883a9164f51fb1c34fdc32d4440d1
paul <paul.sajo.official@gmail.com>
2025-02-16 17:48:20 +0530

    added text effects

--- landing-page/scripts/text-effects.js
+++ landing-page/scripts/text-effects.js
@@ -45,7 +45,7 @@ function loadEffects(data) {
 
     data.forEach((x) => {
         let template = `
-            <div class="tw-w-[450px] tw-h-[400px] tw-p-2 tw-rounded-md tw-overflow-hidden
+            <div class="tw-w-[450px] tw-h-[410px] tw-p-2 tw-rounded-md tw-overflow-hidden
                         tw-border-[1px] tw-border-[#656667] tw-bg-[#171717]">
 
                 <div class="template-preview">
@@ -61,16 +61,24 @@ function loadEffects(data) {
                         on <a href="https://codepen.io">CodePen</a>.</span>
                     </p>
                 </div>
-                <div class="tw-flex tw-min-h-[50px] tw-h-[100px] tw-w-full tw-justify-between tw-place-content-center tw-place-items-center" >
-                    <h2 class="tw-text-xl tw-font-medium">${x.title}</h2>
+                <div class="tw-flex tw-flex-col tw-gap-1 tw-p-1 tw-min-h-[50px] tw-h-[100px] tw-w-full tw-place-content-center tw-place-items-center" >
+                    
+                    <div class="tw-flex tw-justify-between tw-w-full tw-h-full tw-place-content-center tw-place-items-center">
+                        <h2 class="tw-text-xl inter-family tw-uppercase">${x.title}</h2>
 
-                    <a href="${x.codeLink}" target="_blank" rel="noopener noreferrer" 
-                                            class="tw-rounded-full tw-flex tw-border-white tw-border-[1px] 
-                                                    tw-w-[30px] tw-h-[30px] tw-place-content-center
-                                                    tw-text-lg"
-                                            title="open in codepen">        
-                        <i class="bi bi-arrow-up-right"></i>
-                    </a>
+                        <a href="${x.codeLink}" target="_blank" rel="noopener noreferrer" 
+                                                class="tw-rounded-full tw-flex tw-border-white tw-border-[1px] 
+                                                        tw-w-[30px] tw-h-[30px] tw-place-content-center
+                                                        tw-text-lg"
+                                                title="open in codepen">        
+                            <i class="bi bi-arrow-up-right"></i>
+                        </a>
+                    </div>
+                    
+                    <div class="tw-flex tw-justify-between tw-w-full tw-gap-2">
+                        <div class="tw-text-[#444646] tw-place-content-start tw-w-full">by ${x.author}</div>
+                        ${x.interactive === true ? ('<div title="This effect is interactive" class="tw-uppercase tw-place-items-center tw-gap-2 tw-flex"> <div class="tw-w-[5px] tw-h-[5px] tw-bg-green-500 tw-rounded-full"></div> interactive</div>') : ''}
+                    </div>
                 </div>
             </div> 
         `;
